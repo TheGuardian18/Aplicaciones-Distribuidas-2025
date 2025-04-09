e46ca4ba5d0775df1e9ffe4644fc47f7a87e5d9b
TheGuardian18 <yunior_quispe_@hotmail.com>
2025-04-09 08:51:59 -0500

    actualizacion

--- ms_venta/src/main/java/com/example/ms_venta/entity/Venta.java
+++ ms_venta/src/main/java/com/example/ms_venta/entity/Venta.java
@@ -1,9 +1,11 @@
 package com.example.ms_venta.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Venta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
